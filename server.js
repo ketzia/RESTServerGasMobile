c2db824da867849484b1532232827c8f321dde94
ErnestoPM <ernestopm20@gmail.com>
2017-05-06 11:38:25 -0500

    Adding all access

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ app.use(methodOverride('X-HTTP-Method-Override')); // override with the X-HTTP-M
 app.use(function (req, res, next) {
 
     // Website you wish to allow to connect
-    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8100');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     // Request methods you wish to allow
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
